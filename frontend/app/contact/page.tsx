c42f5a51563eb1849539bc5f79e11bef96c3c6d3
Omar El Malak <omarelmalak28@gmail.com>
2025-06-04 12:40:10 -0400

    Setup React env with relevant components + initial routes for landing-related pages

---
+++ frontend/app/contact/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import React, { useEffect, useState } from "react";
+
+const Contact: React.FC = () => {
+    return (
+        <div>
+
+        </div>
+    );
+}
+
+export default Contact;
