4b2c2e893c2fae9107c6c3779b57cca7ddb2c757
usha-sj <ushashasj@gmail.com>
2025-06-19 17:52:31 -0400

    Fixed logo theme issue.

--- frontend/components/landing/Hero.tsx
+++ frontend/components/landing/Hero.tsx
@@ -10,8 +10,8 @@ const Hero: React.FC = () => {
   
       const logo =
       theme === "dark"
-        ? "/ourpathsexp-light.png"
-        : "/ourpathsexp-dark.png";
+        ? "/ourpathsexp-dark.png"
+        : "/ourpathsexp-light.png";
 
   return (
     <section className="w-full bg-var(--background) text-var(--text-light) py-16 px-4 relative overflow-hidden">
